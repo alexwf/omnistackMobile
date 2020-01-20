c9b9262d7225e7ecc5482e2e7c4023a2f0e8b07f
Alex <alexw.f@hotmail.com>
2020-01-19 22:28:55 -0300

    aula 4

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -9,6 +9,7 @@ import api from '../services/api';
 function Main({ navigation }) {
     const [devs, setDevs] = useState([]);
     const [currentRegion, setCurrentRegion] = useState(null);
+    const [techs, setTechs] = useState('');
 
     useEffect(() => {
         async function loadInitialPosition() {
@@ -39,11 +40,11 @@ function Main({ navigation }) {
             params: {
                 latitude,
                 longitude,
-                techs: 'ReactJS'
+                techs
             }
         });
 
-        setDevs(response.data);
+        setDevs(response.data.devs);
     }
 
     function handleRegionChanged(region) {
@@ -57,20 +58,28 @@ function Main({ navigation }) {
     return (
         <>
             <MapView onRegionChangeComplete={handleRegionChanged} initialRegion={currentRegion} style={styles.map}>
-                <Marker coordinate={{ latitude: -26.2831964, longitude: -48.9071056 }} >
-                    <Image style={styles.avatar} source={{ uri: "https://avatars3.githubusercontent.com/u/23398644?s=460&v=4" }} />
+                { devs.map(dev => (
+                    <Marker
+                        key={dev._id}
+                        coordinate={{
+                            longitude: dev.location.coordinates[0],
+                            latitude: dev.location.coordinates[1]
+                        }}
+                    >
+                        <Image style={styles.avatar} source={{ uri: dev.avatar_url }} />
 
-                    <Callout onPress={() => {
-                        navigation.navigate('Profile', { github_username: 'alexwf' });
-                    }}>
-                        <View style={styles.callout}>
-                            <Text style={styles.devName}>Alex Wilbert Friedrichsen</Text>
-                            <Text style={styles.devBio}>É o bicho, uhuuuuuul</Text>
-                            <Text style={styles.devTechs}>ReactJS, React Native, NodeJS</Text>
-                        </View>
-                    </Callout>
+                        <Callout onPress={() => {
+                            navigation.navigate('Profile', { github_username: dev.github_username });
+                        }}>
+                            <View style={styles.callout}>
+                                <Text style={styles.devName}>{dev.name}</Text>
+                                <Text style={styles.devBio}>{dev.bio}</Text>
+                                <Text style={styles.devTechs}>{dev.techs.join(', ')}</Text>
+                            </View>
+                        </Callout>
 
-                </Marker>
+                    </Marker>
+                ))}
             </MapView>
             <View style={styles.searchForm}>
                 <TextInput
@@ -79,6 +88,8 @@ function Main({ navigation }) {
                     placeholderTextColor="#999"
                     autoCapitalize="words"
                     autoCorrect={false}
+                    value={techs}
+                    onChangeText={setTechs}
                 />
 
                 <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
